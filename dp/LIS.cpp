80eba18ffda4a06ad869f8f560d604381be8422d
aokblast <aokblast@aokblast.su>
2021-08-16 16:16:52 +0800

    Chage LIS

--- dp/LIS.cpp
+++ dp/LIS.cpp
@@ -3,7 +3,7 @@ int arr[MAXLEN]; //待处理数组
 int dp[MAXLEN]; //各个结尾的值
 
 void LIS(){
-    for(int i = 0; i < n; ++i) tmp[i] = INT_MAX;
+    for(int i = 0; i <= n; ++i) tmp[i] = INT_MAX;
     tmp[0] = 0;
     for(int i = 0; i < n; ++i){
         int pos = lower_bound(tmp, tmp + n, arr[i]) - tmp; //lower_bound找< // upper_bound找<=
