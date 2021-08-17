f1b5f23467c3714c6a4e26fd08c03c7f23bb1bbf
aokblast <aokblast@aokblast.su>
2021-08-17 16:32:57 +0800

    add minmax to floyd

--- graph/Floyd-Warshal.cpp
+++ graph/Floyd-Warshal.cpp
@@ -1,10 +1,11 @@
-int matrix[MAXLEN][MAXLEN]; 
+int v[MAXLEN][MAXLEN]; 
 //初始化成0x3f3f3f3f，有边的地方为权重，自己为0
 
-for(int i = 0; i < n; ++i){
-    for(int  j = 0; j < n; ++j){
-        for(int  k = 0; k < n; ++k){
-            matrix[i][j] = min(matrix[i][k] + matrix[k][j], matrix[i][j]);
+for(int k = 0; k < n; ++k){
+    for(int  i = 0; i < n; ++i){
+        for(int  j = 0; j < n; ++j){
+            v[i][j] = min(v[i][k] + v[k][j], v[i][j]); //最短路径
+            v[i][j] = min(v[i][j], max(v[i][k], v[k][j])); //最大的最小
         }
     }
 }
