2d6133d3efbd1a98a54a6b0ed204db29a929f97b
aokblast <aokblast@aokblast.su>
2021-10-05 20:52:05 +0800

    add prim

--- graph/mst.cpp
+++ graph/mst.cpp
@@ -20,14 +20,39 @@ void kruskal(){
     for(int i = 0; i < n; ++i){
         int p1 = parent(adjs[i].second.first), p2 = parent(adj[i].second.second); 
         if(p1 == p2) continue;
-        parent[p1] = p2;
+        djs[p1] = p2;
         //一些操作
         if((--cnt) == 1) break;
     }
 }
 
-//Prim 普利姆算法 (待续)
+//Prim 普利姆算法 
 
+vector<pii> adjs[MAXLEN]; //first for 点, second for 权重
+bool visit[MAXLEN];
+
+int n, m;
 void prim(){
+	priority_queue<pii, vector<pii>, greater<pii>> q;
+	int ans = 0; //最小距离
+	q.push({0, 1});
+	int cnt = n; //是否联通
+	while(!q.empty()){
+		pii top = q.top();q.pop();
+		if(visit[top.second])continue;
+		ans += top.first; 
+		visit[top.second] = true;
+		--cnt;
+		for(const pii &adj : adjs[top.second]){
+			if(!visit[adj.first]){
+				q.push({adj.second, adj.first});
+			}
+		}
+	}
+	if(cnt){
+		printf("orz\n");
+	}else{
+		printf("%d\n", ans);
+	}
 
 }
