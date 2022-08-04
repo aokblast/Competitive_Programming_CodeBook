7591910be9e24c7b1eb9d20cf8f62cb3e046cb3c
aokblast <i19780219111@kimo.com>
2022-08-04 23:45:09 +0800

    add CRT

---
+++ math/CRT.cpp
@@ -0,0 +1,27 @@
+// r互素
+
+int a[MAXN], r[MAXN];
+int n = 1, ans = 0;
+
+
+for(int i = 0; i < k; ++i)
+	n *= r[i];
+
+for(int i = 0; i < k; ++i){
+	ll m = n / r[i];
+	exgcd(m, r[i]);
+	ans = (ans + ((((lll)a[i] * m) % n) * x % n)) % n;
+}
+
+
+// r非互素
+
+ll	M = r[0],  ans = a[0];
+
+for(int i = 1; i < k; ++i){
+	ll da = (((a[i] - ans) % r[i]) + r[i]) % r[i];
+	ll g = exgcd(M, r[i]);
+	auto tmp = (lll)M * x * (da / g);
+	M = M * (r[i] / g);
+	ans = (((ans + tmp) % M) + M) % M;
+}
