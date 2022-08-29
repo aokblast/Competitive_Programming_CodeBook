e3e89cfe536a6bf57b1efd5003245f9d093d72b8
aokblast <i19780219111@kimo.com>
2022-08-30 00:02:20 +0800

    add CRT limitation

--- math/CRT.cpp
+++ math/CRT.cpp
@@ -21,6 +21,8 @@ ll	M = r[0],  ans = a[0];
 for(int i = 1; i < k; ++i){
 	ll da = (((a[i] - ans) % r[i]) + r[i]) % r[i];
 	ll g = exgcd(M, r[i]);
+	if(da % g)
+		return -1;
 	auto tmp = (lll)M * x * (da / g);
 	M = M * (r[i] / g);
 	ans = (((ans + tmp) % M) + M) % M;
